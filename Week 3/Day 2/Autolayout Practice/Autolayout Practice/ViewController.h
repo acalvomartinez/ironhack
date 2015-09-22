5571672d31c34ea7dc0d9a79dbb8577952ef04fd
Toni <toniconmac@gmail.com>
2015-09-22 14:10:24 +0200

    Week 3. Day 2. Autolayout Practice

---
+++ Week 3/Day 2/Autolayout Practice/Autolayout Practice/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Autolayout Practice
+//
+//  Created by Toni on 22/09/15.
+//  Copyright © 2015 Toni. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Week 3/Day 2/Autolayout Practice/Autolayout Practice/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Autolayout Practice
+//
+//  Created by Toni on 22/09/15.
+//  Copyright © 2015 Toni. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
