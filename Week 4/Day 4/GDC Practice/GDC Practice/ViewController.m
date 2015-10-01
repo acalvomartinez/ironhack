0f25998b93075168b97db276397febe6a2bbbffa
Toni <toniconmac@gmail.com>
2015-10-01 11:43:22 +0200

    Week 4. Day 4. GDC Practice

--- Week 4/Day 4/GDC Practice/GDC Practice/ViewController.m
+++ Week 4/Day 4/GDC Practice/GDC Practice/ViewController.m
@@ -28,23 +28,35 @@
     [super viewDidLoad];
     
     
+//    
+//    [[DownloadManager sharedInstance] downloadImageAtURL:@"http://img.lum.dolimg.com/v1/images/Darth-Vader_6bda9114.jpeg" completion:^(UIImage *image) {
+//        self.imageView1.image = image;
+//    }];
+//    
+//    [[DownloadManager sharedInstance] downloadImageAtURL:@"http://thestarwarstrilogy.com/StarWars/wallpaper/Original-Trilogy-Darth-Vader/Original%20Trilogy%20-%20Darth%20Vader%2005.jpg" completion:^(UIImage *image) {
+//        self.imageView2.image = image;
+//    }];
+//    
+//    [[DownloadManager sharedInstance] downloadImageAtURL:@"http://images1.wikia.nocookie.net/__cb20111108044044/swcp/es/images/e/e8/VaderFather.jpg" completion:^(UIImage *image) {
+//        self.imageView3.image = image;
+//    }];
+//    
+//    [[DownloadManager sharedInstance] downloadImageAtURL:@"http://i1.cdnds.net/14/05/618x402/darth-vader.jpg" completion:^(UIImage *image) {
+//        self.imageView4.image = image;
+//    }];
     
-    [[DownloadManager sharedInstance] downloadImageAtURL:@"http://img.lum.dolimg.com/v1/images/Darth-Vader_6bda9114.jpeg" completion:^(UIImage *image) {
-        self.imageView1.image = image;
-    }];
-    
-    [[DownloadManager sharedInstance] downloadImageAtURL:@"http://thestarwarstrilogy.com/StarWars/wallpaper/Original-Trilogy-Darth-Vader/Original%20Trilogy%20-%20Darth%20Vader%2005.jpg" completion:^(UIImage *image) {
-        self.imageView2.image = image;
-    }];
-    
-    [[DownloadManager sharedInstance] downloadImageAtURL:@"http://images1.wikia.nocookie.net/__cb20111108044044/swcp/es/images/e/e8/VaderFather.jpg" completion:^(UIImage *image) {
-        self.imageView3.image = image;
-    }];
-    
-    [[DownloadManager sharedInstance] downloadImageAtURL:@"http://i1.cdnds.net/14/05/618x402/darth-vader.jpg" completion:^(UIImage *image) {
-        self.imageView4.image = image;
-    }];
-    
+    [self test];
+}
+
+- (void)test {
+    dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0);
+    dispatch_async(queue, ^{
+        [self performSelector:@selector(log) withObject:self afterDelay:2.0];
+    });
+}
+
+- (void)log {
+    NSLog(@"Hello World!");
 }
 
 - (void)didReceiveMemoryWarning {
