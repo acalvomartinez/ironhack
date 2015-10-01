5e6ab4136a1ccd061bc4afe3397fca5d00b3cbb6
Toni <toniconmac@gmail.com>
2015-10-01 11:11:06 +0200

    Week 4. Day 4. GDC with singleton Practice

--- Week 4/Day 4/GDC Practice/GDC Practice/ViewController.m
+++ Week 4/Day 4/GDC Practice/GDC Practice/ViewController.m
@@ -8,6 +8,8 @@
 
 #import "ViewController.h"
 
+#import "DownloadManager.h"
+
 //http://img.lum.dolimg.com/v1/images/Darth-Vader_6bda9114.jpeg
 //http://images.esellerpro.com/2486/I/356/6/magnet-star-wars-darth-vader.jpg
 //http://images1.wikia.nocookie.net/__cb20111108044044/swcp/es/images/e/e8/VaderFather.jpg
@@ -18,8 +20,6 @@
 @property (weak, nonatomic) IBOutlet UIImageView *imageView2;
 @property (weak, nonatomic) IBOutlet UIImageView *imageView3;
 @property (weak, nonatomic) IBOutlet UIImageView *imageView4;
-
-@property (strong, nonatomic) dispatch_queue_t download_q;
 @end
 
 @implementation ViewController
@@ -27,34 +27,24 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    [self presentImage:@"http://img.lum.dolimg.com/v1/images/Darth-Vader_6bda9114.jpeg" intoImageView:self.imageView1];
-    [self presentImage:@"http://thestarwarstrilogy.com/StarWars/wallpaper/Original-Trilogy-Darth-Vader/Original%20Trilogy%20-%20Darth%20Vader%2005.jpg" intoImageView:self.imageView2];
-    [self presentImage:@"http://images1.wikia.nocookie.net/__cb20111108044044/swcp/es/images/e/e8/VaderFather.jpg" intoImageView:self.imageView3];
-    [self presentImage:@"http://i1.cdnds.net/14/05/618x402/darth-vader.jpg" intoImageView:self.imageView4];
     
-}
-
-- (dispatch_queue_t)download_q {
-    if(!_download_q) {
-        _download_q = dispatch_queue_create("com.ironhack.process.donwload", DISPATCH_QUEUE_SERIAL);
-    }
-    return _download_q;
-}
-
-- (void)presentImage:(NSString *)urlString intoImageView:(UIImageView *)imageView {
-    dispatch_async(self.download_q, ^{
-        UIImage *image = [self donwloadImage:urlString];
-        dispatch_async(dispatch_get_main_queue(), ^{
-            imageView.image = image;
-        });
-    });
     
-}
-
-- (UIImage *)donwloadImage:(NSString *)urlString {
-    NSURL *url  =[NSURL URLWithString:urlString];
-    NSData *imageData = [NSData dataWithContentsOfURL:url];
-    return [UIImage imageWithData:imageData];
+    [[DownloadManager sharedInstance] downloadImageAtURL:@"http://img.lum.dolimg.com/v1/images/Darth-Vader_6bda9114.jpeg" completion:^(UIImage *image) {
+        self.imageView1.image = image;
+    }];
+    
+    [[DownloadManager sharedInstance] downloadImageAtURL:@"http://thestarwarstrilogy.com/StarWars/wallpaper/Original-Trilogy-Darth-Vader/Original%20Trilogy%20-%20Darth%20Vader%2005.jpg" completion:^(UIImage *image) {
+        self.imageView2.image = image;
+    }];
+    
+    [[DownloadManager sharedInstance] downloadImageAtURL:@"http://images1.wikia.nocookie.net/__cb20111108044044/swcp/es/images/e/e8/VaderFather.jpg" completion:^(UIImage *image) {
+        self.imageView3.image = image;
+    }];
+    
+    [[DownloadManager sharedInstance] downloadImageAtURL:@"http://i1.cdnds.net/14/05/618x402/darth-vader.jpg" completion:^(UIImage *image) {
+        self.imageView4.image = image;
+    }];
+    
 }
 
 - (void)didReceiveMemoryWarning {
